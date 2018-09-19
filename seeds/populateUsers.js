1a6a1d36cb972f585ff524226ed63a40138d773d
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-09-20 11:15:17 +1200

    created table and seed for found

--- seeds/populateUsers.js
+++ seeds/populateUsers.js
@@ -12,7 +12,21 @@ exports.seed = function(knex, Promise) {
           user_name: 'symesharr',
           email_address: 'harrison@devacademy.co.nz',
           contact_details: 'Ring the bell ;)',
-          hash: hashSync('Krang', saltRounds)
+          hash: hashSync('bob', saltRounds)
+        },
+        {
+          id: 2,
+          user_name: 'bob',
+          email_address: 'bob@devacademy.co.nz',
+          contact_details: 'Call',
+          hash: hashSync('krunk', saltRounds)
+        },
+        {
+          id: 3,
+          user_name: 'maurice',
+          email_address: 'maurice@gmail.com',
+          contact_details: 'Hope',
+          hash: hashSync('123', saltRounds)
         }
       ]);
     });
