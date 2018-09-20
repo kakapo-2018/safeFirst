d35bbcb015d7f4ba3b35e3b8e6e7aa8538cac46e
student <richardboeve@hotmail.com>
2018-09-21 09:45:45 +1200

    Make App stateful

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,42 +5,56 @@ import {connect} from 'react-redux'
 import Login from './Login'
 import Register from './Register'
 import Nav from './Nav'
-import Upload from './Upload'
-import List from './List'
-//import Header from './Header'
+import {getFound} from '../actions/found'
+import {getLost} from '../actions/lost'
 
 
-const App = ({auth}) => (
-  <Router>
-    <div className="container has-text-centered">
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      auth: auth,
+    }
+  }
 
-      <div className="hero is-small is-primary">
-        <div className="hero-body has-text-centered">
-          <Link to='/' className="">
-            <h1 className="title is-1">Lost and Found</h1>
-          </Link>
-          <Nav />
+  componentDidMount() {
+    this.props.dispatch(getFound())
+    this.props.dispatch(getLost())
+  }
+  
+  render() {
+    return ( 
+      <Router>
+        <div className="container has-text-centered">
+    
+          <div className="hero is-small is-primary">
+            <div className="hero-body has-text-centered">
+              <Link to='/' className="">
+                <h1 className="title is-1">Lost and Found</h1>
+              </Link>
+              <Nav />
+            </div>
+          </div> 
+    
+          <div className=''>
+            {!auth.isAuthenticated &&
+              <Route exact path="/" component={Login} />
+            }
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/upload" component={Upload} />
+            <Route path="/list" component={List} />
+          </div>
+    
         </div>
-      </div> 
+      </Router>
+    )
+  }
+}
 
-
-      <div className=''>
-        {!auth.isAuthenticated &&
-          <Route exact path="/" component={Login} />
-        }
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/upload" component={Upload} />
-        <Route path="/list" component={List} />
-      </div>
-
-    </div>
-  </Router>
-)
-
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = (state) => {
   return {
-    auth
+    auth: state.auth
   }
 }
 
