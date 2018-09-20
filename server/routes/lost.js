41725b13ff94cb1a5297802322a9656c5e619802
student <winstonshred@gmail.com>
2018-09-21 10:12:46 +1200

    fixed catch error in routes lost file

--- server/routes/lost.js
+++ server/routes/lost.js
@@ -3,20 +3,15 @@ var router = require('express').Router()
 router.post('/', lostRoute)
 router.get('/', getLostAnimalsRoute)
 
-// var createLostAnimal = require('../db/lostAnimals')
-// var getLostAnimals = require('../db/lostAnimals')
-
 var { createLostAnimal, getLostAnimals } = require('../db/lostAnimals')
 
-
-function lostRoute(req, res) {
+function lostRoute(req, res, next) {
 	const { name, species, photo } = req.body
 	createLostAnimal(name, species, photo)
 		.then(() => next())
 		.catch(err => {
 			res.status(500).send({ message: "Server Error" })
 		})
-		.catch(err => res.status(500).send({ message: "Server Error" }))
 }
 
 function getLostAnimalsRoute(req, res) {
