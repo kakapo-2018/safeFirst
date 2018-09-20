b3e2f1850f5d941bb0359446ae42113477fc24e9
nepcam <cameron.nepe@gmail.com>
2018-09-21 10:43:02 +1200

    component with function list

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {registerUserRequest} from '../actions/register'
 import {loginError} from '../actions/login'
-
+import LostList from './LostList'
+import FoundList from './FoundList'
 
 const List = (props) => {
   return (
     <div className='foundlist'>
       <p className='welcome'>List page</p>
-   
+      <h1>this is the lost list</h1>
+      <LostList/>
+      <h1>this is the found list list</h1>
+      <FoundList/>
     </div>
   )
 }
--- client/components/LostList.jsx
+++ client/components/LostList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import LostListItem from './LostListItem'
 
 // import LostList from '../../seeds/lost' // fill in once we get the array name
 
@@ -7,6 +7,10 @@ const LostList = (props) => {
     return (
       <div className='lostlist'>
         <p className='welcome'>Lost Animals</p>
+        <LostListItem/>
+        <p>{props.name}</p>
+        {console.log(props.name)}
+
         {/* {props.lost.map(lost => {  // need the name of our array in db
           return (
             <LostListItem key={lost.id}/>
