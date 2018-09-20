578e93307bc6526c2db85f57fbd72753d336fb2d
Nat <nat_sirisamphan@hotmail.com>
2018-09-21 10:38:30 +1200

    pages linking

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {registerUserRequest} from '../actions/register'
 import {loginError} from '../actions/login'
+import {Link} from 'react-router-dom'
 
-
-const List = (props) => {
+const List = () => {
   return (
     <div className='foundlist'>
       <p className='welcome'>List page</p>
-   
+      <button><Link to='/upload'>Upload Missing Animal</Link></button>
     </div>
   )
 }
