2be3c648370f87a4190afd5a083a5d77d8b9abaa
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-09-20 16:19:37 +1200

    get found gives and array of object{animal} back

--- server/routes/found.js
+++ server/routes/found.js
@@ -1,6 +1,6 @@
 var router = require('express').Router()
 
-var {animalFound, foundExists, createFound} = require('../db/found')
+var {getFoundDB, foundExists, createFound} = require('../db/found')
 
 router.post('/', postFound)
 
@@ -10,36 +10,28 @@ function postFound (req, res, next) {
   const user_id = req.body.user_id
 
   const newLost = {species, photo, user_id}
-//   console.log(newLost)
-
     createFound (species, photo)
-        .then(() => {
-            console.log('then then then');
-            next()
-        })
+        .then(() => next())
         .catch(err => {
           res.status(500).send({message: "Server Error in found exist"})
         })
-    // })
     .catch(err => res.status(500).send({message: "Server Error full catch"}))
 }
 
-getFound = (req, res, next) => {
-    const id = req.body.id
-    const species = req.body.species
-    const photo = req.body.photo
-    const user_id = req.body.user_id
 
-  const newLost = {species, photo, user_id}
-    animalFound(id)
-    .then(exists => {
-      if (exists) return res.json(newLost)
-      .then(() => next())
-        .catch(err => {
-          res.status(500).send({message: "Server Error"})
-        })
+getFound = (req, res, next) => {
+    // const id = req.body.id
+    // const species = req.body.species
+    // const photo = req.body.photo
+    // const user_id = req.body.user_id
+    
+    // const newLost = {species, photo, user_id}
+    getFoundDB()
+    .then(animal => {
+        console.log(animal)
+        res.json(animal)
     })
-    .catch(err => res.status(500).send({message: "Server Error"}))
+    .catch(err => res.status(500).send({message: "Server Error 2"}))
 }
 
 router.get('/', getFound)
