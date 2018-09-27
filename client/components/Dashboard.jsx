d1d14153402716a3a5a31078a946bb7707b35072
nepcam <cameron.nepe@gmail.com>
2018-09-28 09:27:00 +1200

    updated dashboard

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = (props) => (
         <div>
             <h1>
-                I am a dashboard
+                I am a dashboard, pull SafetyMeet, HazardsDay and Incident / IncidentList
             </h1>
         </div>
 
