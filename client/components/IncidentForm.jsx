73744efef06328867f8191247cb785d8c3b581d9
nepcam <cameron.nepe@gmail.com>
2018-09-27 18:45:01 +1200

    setting up components to show something

---
+++ client/components/IncidentForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IncidentForm = (props) => (
+        <div>
+            <h1>
+                I am a incident / accident form
+            </h1>
+        </div>
+
+)
+    
+export default IncidentForm
