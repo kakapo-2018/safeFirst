6e96789a08822ba2cf25604d6bec38c0c9b13c18
robjwilkinson@hotmail.com <robert@Roberts-MacBook-Air.local>
2018-09-29 11:51:36 +1200

    HazardsDay form setup

--- client/components/HazardsDay.jsx
+++ client/components/HazardsDay.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 
 const HazardsDay = (props) => (
-        <div>
-            <h1>
-                I am daily pre-start meeting page
-            </h1>
-        </div>
+    <div>
+        <h1>
+            I am daily pre-start meeting page
+        </h1>
+    </div>
 
-)
+
+class HazardsDay extends React.Component    {
+    constructor(props)  {
+        super(props)
+        this.state = {
+            
+        }
+    }
+}
+
+onsubmit()  {
+    e.preventDefault()
+    console.log(this.state);
+}
+
+render()    {
+    return  (
+        <form onSubmit={}>
+            <input className="input" type="text" name="Hazard">
+            </input>
+            <input className="input" type="text" name="Risk">
+            </input>
+            <input className="input" type="text" name="Control">
+            </input>
+            <input className="input" type="text" name="Date">
+            </input>
+            <input className="button" type="submit">
+            </input>
+        </form>
+    ) 
+}
+}
     
 export default HazardsDay
