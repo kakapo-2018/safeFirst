ba560fe3b816f3d6f7ca815da32f45ae10c751d7
nepcam <cameron.nepe@gmail.com>
2018-09-28 16:05:03 +1200

    working on components

--- client/components/HazardForm.jsx
+++ client/components/HazardForm.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
+class HazardForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            crew: '',
+            hazard: '',
+            risk: 1,
+            control: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(event) {
+        this.setState({value: event.target.name})
+    }
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value)
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
 const HazardForm = (props) => (
         <div>
             <h1>
@@ -8,5 +35,7 @@ const HazardForm = (props) => (
         </div>
 
 )
+
+
     
 export default HazardForm
