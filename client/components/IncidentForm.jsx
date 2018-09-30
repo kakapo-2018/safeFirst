94d0d4fb2aeb530d169bf1bab2192e6526a6cf35
Richard-Boeve <richardboeve@hotmail.com>
2018-09-30 13:44:11 +1300

    incidentform

--- client/components/IncidentForm.jsx
+++ client/components/IncidentForm.jsx
@@ -4,11 +4,20 @@ class IncidentForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            value: ''
+            person_involved: '',
+            incident_type: '',
+            date_time: '',
+            task: '',
+            other_task: '',
+            incident: '',
+            other_incident: '',
+            location: '',
+            first_aid: '',
         }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.refreshForm = this.refreshForm.bind(this)
     }
 
     handleChange(event) {
@@ -21,14 +30,26 @@ class IncidentForm extends React.Component {
         event.preventDefault()
     }
 
+    refreshForm(){
+        this.setState({            
+        })
+        this.render()
+    }
+
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
+                <label>Person involved:<input type="text" name="person_involved" onChange={this.handleChange} /></label><br />
+                <label>Incident type:<input type="text" name="inciden_type" onChange={this.handleChange} /></label><br />
+                <label>Date & Time:<input type="text" name="date_time" onChange={this.handleChange} /></label><br />
+                <label>Task:<input type="text" name="task" onChange={this.handleChange} /></label><br />
+                <label>Other task:<input type="text" name="other_task" onChange={this.handleChange} /></label><br />
+                <label>Incident:<input type="text" name="incident" onChange={this.handleChange} /></label><br />
+                <label>Other incidident:<input type="text" name="other_incident" onChange={this.handleChange} /></label><br />
+                <label>Location:<input type="text" name="location" onChange={this.handleChange} /></label><br />
+                <label>First aid:<input type="text" name="first_aid" onChange={this.handleChange} /></label><br />
                 <input type="submit" value="Submit" />
+                {/* <button onClick={this.refreshForm}>Refresh</button> */}
             </form>
         )
     }
