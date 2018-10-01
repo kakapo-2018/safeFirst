fe72bc5bb8b651709260226335eec811cacdaa85
nepcam <cameron.nepe@gmail.com>
2018-10-01 15:12:37 +1300

    working on SafetyMeet & Login

--- client/components/SafetyMeet.jsx
+++ client/components/SafetyMeet.jsx
@@ -36,36 +36,38 @@ class SafetyMeet extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-            <h1>Safety Meeting</h1>
-            <div class="columns">
-                <div class="column is-half">
-                        <label class="label">Meeting ID:   </label>
-                            <  input class="input" type="text" name="id" onChange={this.handleChange} />
-                    </div>
-                    <div class="field">
-                        <label class="label">Date - Time:   </label>
-                            <  input class="input" type="text" name="date_time" onChange={this.handleChange} />
-                    </div>
-                    <div class="field">
-                        <label class="label">Location:   </label>
-                            <  input class="input" type="text" name="location" onChange={this.handleChange} />
-                    </div>
-                    <div class="field">
-                        <label class="label">Discussion:</label>
-                        <p>Health & Safety concerns, training needs, incidents/accident trends, personal   </p>
-                            <div class="control">
-                                <textarea class="textarea" name="issues" rows="15" onChange={this.handleChange}></textarea>
+                <h1>Safety Meeting</h1>
+                <div className="column is-left">
+                        <div className="column is-one-fifth">
+                            <label className="label">Meeting ID:   </label>
+                                <input className="input" type="text" name="id" onChange={this.handleChange} />
+                        </div> 
+                        <div className="column is-two-fifths">
+                            <label className="label">Date - Time:   </label>
+                                <input className="input" type="text" name="date_time" onChange={this.handleChange} />
+                        </div> 
+                        <div className="column is-two-fifths">
+                            <label className="label">Location:   </label>
+                                <input className="input" type="text" name="location" onChange={this.handleChange} />
+                        </div>
+                        <div className="column is-two-thirds">
+                            <label className="label">Discussion:</label>
+                                <div className="control">
+                                    <textarea className="textarea" name="issues" placeholder="Attendees, Health & Safety concerns, training needs, incidents/accident trends, personal concerns " rows="10" onChange={this.handleChange}></textarea>
+                                </div>
+                        </div>
+                        <div className="field is-grouped">
+                            <div className="control">
+                                <button className="button is-link">Submit</button>
                             </div>
-                    </div>
-                    <div class="field is-grouped">
-                        <div class="control">
-                            <button class="button is-link">Submit</button>
+                            <div className="control">
+                                <button className="button is-link">Cancel</button>
+                            </div>
+                            <div className="control">
+                                <button className="button is-link">Add Hazard</button>
+                            </div>
                         </div>
-                        <div class="control">
-                            <button class="button is-text">Cancel</button>
-                        </div>
                     </div>
-                </div>
             </form>
         )
     }
