61542da9e6571bade90cb53574027c03ca33fdb1
nepcam <cameron.nepe@gmail.com>
2018-10-02 13:31:22 +1300

    tried to add Nav to Dashboard - unsuccessful

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 import HazardsDay from './HazardsDay'
 import Statistics from './Statistics'
-
+import DashboardHeader from './DashboardHeader'
 
 
 
